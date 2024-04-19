a96c81b23cdddf2fdd0aefbec1076c1f3fab499e
Steve Chen <chensteve520@gmail.com>
2024-04-19 15:46:37 -0700

    deployment

--- server.cjs
+++ server.cjs
@@ -5,6 +5,7 @@ const share = require("./backend/share.api.cjs");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 require('dotenv').config()
+const path = require('path');
 
 const app = express();
 
@@ -24,6 +25,14 @@ app.use("/api/user", user);
 app.use("/api/urlPassword", urlPassword);
 app.use("/api/share", share);
 
-app.listen(8000, function () {
+let frontend_dir = path.join(__dirname, 'dist')
+
+app.use(express.static(frontend_dir));
+app.get('*', function (req, res) {
+    console.log("received request");
+    res.sendFile(path.join(frontend_dir, "index.html"));
+});
+
+app.listen(process.env.PORT || 8000, function () {
 	console.log("Starting app now...");
 });
